05686c01bd9f29757f4750b6c2d4f69978887cd2
Roel Franken <roelfranken@gmail.com>
2015-05-31 15:13:22 +0200

    Meterplaatsing met standen en aanroep proc

--- app/meterplaatsing.js
+++ app/meterplaatsing.js
@@ -42,7 +42,7 @@ angular.module('mtpApp', [
 
     .factory('metersPlacementResource', function ($resource) {
         "use strict";
-        return $resource('http://wlo1.open-i.nl:8888/rest/fine/'+ 'finecr$meter_placements'+ '/:id/',
+        return $resource('http://wlo1.open-i.nl:8888/rest/fine/_proc/TEST',
             {
                 app_name    : 'Fine'
             },
@@ -51,6 +51,7 @@ angular.module('mtpApp', [
                 delete: {method: 'DELETE'}
             });
     })
+
 
     .factory('meterReadingsResource', function ($resource) {
         "use strict";
@@ -185,14 +186,16 @@ angular.module('mtpApp', [
         $scope.selectMeter = function(index) {
             $scope.meters.selectedRow = $scope.meters.recordSet.record[index];
             console.log('Index:' + index);
+            $scope.counters.executeQuery();
             window.location.href = "#/meteropnamen";
         }
 
 
         //        ------------------- meters reading + meter placement ------------------------
 
-        $scope.meterPlacements = {};
-        $scope.meterReadings = {};
+        $scope.meterPlacements      = {};
+        $scope.meterReadings        = {};
+        $scope.meterReadingDetails  = {};
         $scope.meterReadings.record = {}
         $scope.meterReadings.insertInOperation = false;
         $scope.meterReadings.insertSuccessful = false;
@@ -202,68 +205,102 @@ angular.module('mtpApp', [
         $scope.counters = {};
         $scope.countersParamsObj = {}
 
+        $scope.stand= {}
+        $scope.stand.pattern = new RegExp("^[0-9]*$"); // alleen numeriek
         $scope.counters.executeQuery = function(){
-        $scope.countersParamsObj.fields   = 'COP_NAME,WHEELS,DECIMALS,CTG_ID'
-        // Haal de telwerkeen op van de categorie van de gekozen meter:
-        var filter = 'CTG_ID = ' + $scope.meters.selectedRow.CTG_ID;
-        $scope.countersParamsObj.filter = filter;
-        $scope.counters.recordSet = countersResource.query($scope.countersParamsObj
-            , function(result){
-                $scope.queryInOperation = false;
-                console.log('succes!')
-            }
-            , function(errorResult) {
-                $scope.queryInOperation = false;
-                console.log('Fout:' + errorResult.data.error[0].message);
-                $scope.errorResource = errorResult.data.error[0];
-            }
+            $scope.countersParamsObj.fields   = 'COP_ID,COP_NAME,WHEELS,DECIMALS,CTG_ID,MRT_AUD_DATE_CREATED,STAND'
+            // Haal de telwerkeen op van de categorie van de gekozen meter:
+            var filter = 'CTG_ID = ' + $scope.meters.selectedRow.CTG_ID;
+            $scope.countersParamsObj.filter = filter;
+            $scope.counters.recordSet = countersResource.query($scope.countersParamsObj
+                , function(result){
+                    $scope.queryInOperation = false;
+                    console.log('succes!')
+                }
+                , function(errorResult) {
+                    $scope.queryInOperation = false;
+                    console.log('Fout:' + errorResult.data.error[0].message);
+                    $scope.errorResource = errorResult.data.error[0];
+                }
         )};
 
-        // Maak een METER PLACEMENT aan:
-        // Deze moet eerst omdat hier fouten door gebruikersinvoer kunnen optreden:
-        $scope.meterPlacements.register = function () {
-            $scope.meterReadings.insertInOperation = true; // throbber aanzetten:
+
+
+        $scope.createMeterPlacement = function () {
+            // Roep Stored Procedure aan om:
+            // Een METER PLACEMENT aan te maken
+            // Een METER READING aan te maken
+            // Een of meerdere METER READING DETAILS aan te maken:
+
+            // METER PLACEMENT record samenstellen:
+            $scope.createMeterPlacement.InOperation= true; // throbber GIFje aanzetten
             $scope.meterPlacements.record = {};
             $scope.meterPlacements.record.CCN_ID    = $scope.connections.selectedRow.ID;
             $scope.meterPlacements.record.MTR_ID    = $scope.meters.selectedRow.ID;
+            // Uit scherm:
             $scope.meterPlacements.record.STARTDATE = $scope.meterReadings.record.RECORDING_DATE;
             $scope.meterPlacements.record.MTP_TYPE  = 'MW';
-            metersPlacementResource.save( $scope.meterPlacements.record,
+
+            // METER READINGS record samenstellen:
+            $scope.meterReadings.record.MTR_ID         = $scope.meters.selectedRow.ID;
+            $scope.meterReadings.record.REASON         = 'PLAATSING';
+            // Uit scherm:
+            //$scope.meterReadings.record.RECORDING_DATE
+            //$scope.meterReadings.record.METHOD
+            //
+            // De METER READING DETAILS zitten in een array van objecten:
+            for (var i = 0; i < $scope.counters.recordSet.record.length; i++) {
+                 delete $scope.counters.recordSet.record[i].$$hashKey;
+            }
+
+            $scope.meterReadingDetails.array =   $scope.counters.recordSet.record;
+
+
+            // Dit is de inhoud in JSON die als VALUE moet worden aangeboden:
+            var valueJSON = {  mtp : $scope.meterPlacements.record
+                                  ,  mrd : $scope.meterReadings.record
+                                  ,  mrt : $scope.meterReadingDetails.array
+            };
+
+            // Als String notatie
+            var valueString         = JSON.stringify(valueJSON);
+            // En tenslotte escaped omdat anders de Stored Procedure in de war raakt:
+            var valueStringEscaped  = valueString.replace(/"/g,'\\"') //goed  // Deze werkt:
+
+
+            //
+            var aanroep = {};
+
+            aanroep.params = [
+                {
+                    name       : "P_1",
+                    value      : valueStringEscaped
+                }
+            ];
+
+            metersPlacementResource.save( aanroep,
                 function(result) {
                     //handle success
-                    // Maak de meterplaatsing aan:
-                    $scope.meterReadings.register();
+                    // Throbber uitzetten...
+                    $scope.createMeterPlacement.InOperation = false;
+                    // toon OK symbool:
+                    $scope.createMeterPlacement.Successful  = true;
+                    // ...en toon en vul het counters blok:
+                   $scope.counters.executeQuery();
                 },
                 function(errorResult) {
-                    $scope.meterReadings.insertInOperation = false;
-                    $scope.meterReadings.insertSuccessful = false;
+                    // Throbber uitzetten...
+                    $scope.createMeterPlacement.InOperation = false;
+                    $scope.createMeterPlacement.Successful = false;
+                    // Vul fout:
                     $scope.errorResource = errorResult.data.error[0];
                 })
+
+
 
         };
 
-        // Maak een METER READING aan:
-        $scope.meterReadings.register = function() {
-            $scope.meterReadings.record.ID     = 9999;// Tja, Dreamfactory kan geen door de DB gegenereerde ID teruggeven(?!), dus daarom maar zo.
-            $scope.meterReadings.record.MTR_ID = $scope.meters.selectedRow.ID;
-            $scope.meterReadings.record.REASON = 'PLAATSING';
-            meterReadingsResource.save( $scope.meterReadings.record.ID, $scope.meterReadings.record,
-                function(result) {
-                    //handle success
-                    // Alles is goed gegaan:
-                    // Throbber uitzetten...
-                    $scope.meterReadings.insertInOperation = false;
-                    // ...en toon en vul het counters blok:
-                    $scope.meterReadings.insertSuccessful = true;
-                    $scope.counters.executeQuery();
-                },
-                function(errorResult) {
-                    $scope.meterReadings.insertInOperation = false;
-                    $scope.meterReadings.insertSuccessful  = false;
-                    $scope.errorResource = errorResult.data.error[0];
-                    throw exception; // hier stoppen!
-                })
-        }
+
 
 
 
